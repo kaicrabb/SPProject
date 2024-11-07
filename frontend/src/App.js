34711ef2927400da105845d3771679d4c16113c0
Crabb <S550248@nwmissouri.edu>
2024-11-06 18:16:57 -0600

    Revert "small change"
    This reverts commit b2bdc8740bbe43fa3b058cd31d1ded90990ec157.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SignUp from './signup/signup'; // Assuming you have a SignUp component
 import Login from './login/login'; // Assuming you have a Login component
 
 function App() {
   return (
-    <Router> {/* Make sure this is here to wrap your routes */}
+    <Router>
       <div className="App">
+        {/* Main page content */}
         <h1>Welcome to Our Application</h1>
         <p>Click below to either Sign Up or Log In:</p>
-
+        
         {/* Buttons for navigation */}
         <div>
           <Link to="/signup">
